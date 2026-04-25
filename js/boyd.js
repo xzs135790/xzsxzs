5f1f828a6a6c2a3380fc19c27bc0956f640fc586
xzs135790 <3639676338@qq.com>
2026-04-25 13:35:37 +0800

    Delete js directory

--- js/boyd.js
+++
@@ -1,7 +0,0 @@
-const tan = document.querySelector('.tan');
-const tan_ann = document.getElementById('tan_ann');
-tan.style.display = 'block';
-tan_ann.addEventListener('click', function () {
-    tan.style.display = 'none';
-
-})
